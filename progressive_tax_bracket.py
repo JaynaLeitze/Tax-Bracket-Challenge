366ce314ec014b7dd07cad71b621fd9e7ed98ac1
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-09 18:55:08 -0600

    added comments on initial .py file

--- progressive_tax_bracket.py
+++ progressive_tax_bracket.py
@@ -8,3 +8,38 @@
 # 1. Assume this application will be used by a 3rd party tax consultant who will have to run this for 100 clients.  
 #    Write a program that is scalable.
 #    The program should take the $ income and return the tax amount.
+
+#Define variables of income brackets:
+fifty = 50000
+twenty = 20000
+ten = 10000
+
+#Define variables that compute tax 
+tax_on_10k = 0
+tax_on_20k = int((20000 - 10000)*0.1)
+tax_on_50k = int((50000-30000)*0.2) + tax_on_20k
+
+#Define a function that takes income amount as a parameter and returns tax amount on income given
+def tax(income):
+    #set initial tax to zero
+    tax_amount = 0
+    #conditionally render tax for income greater than fifty
+    if income > fifty:
+        tax_amount += int((income - fifty)*0.3 + tax_on_50k)
+    #conditionally render tax for income greater than 20, less than 50
+    elif income >twenty:
+        tax_amount += int((income-twenty)*0.1 +tax_on_20k)
+    #conditionally render tax for income greater than 20, less than 20
+    elif income > ten:
+        tax_amount += int((income - ten) + tax_on_10k)
+    #return tax amount
+    return(tax_amount)
+
+#test output with print statements
+print(tax(30000))
+print(tax(75000))
+print(tax(15000))
+print(tax(10000))
+
+#This file helped me to think through the problem itself. 
+#Variables are clearly defined, but not very clean and concise
