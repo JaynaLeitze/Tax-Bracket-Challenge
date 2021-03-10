2c74520935dcdc793c835e47e1b852f456b37eb3
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-10 07:40:22 -0600

    create customer model and accountant model

---
+++ models.py/accountant.py
@@ -0,0 +1,6 @@
+class Accountant:
+    def __init__(self, name, id,):
+        self.id = id
+        self.name = name
+        
+    
---
+++ models.py/customer.py
@@ -0,0 +1,10 @@
+class Customer():
+
+    def __init__(self, id, name, address,income,accountant_id, email = "", password = ""):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.income = income
+        self.accountant_id = accountant_id
+        self.email = email
+        self.password = password
